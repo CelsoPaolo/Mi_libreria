8216034147c84fa2495abca04b71b728c0e0d54e
CelsoPaolo <velascopaolo.2920@gmail.com>
2025-09-11 20:42:33 -0400

    agregando libreria: Una metrica de inventario atomica

---
+++ mi_libreria_inventario/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='inventario-libreria-atomica',
+    version='0.1',
+    author='Celso Paolo',
+    description='Una librería para cálculos atómicos de inventario.',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
